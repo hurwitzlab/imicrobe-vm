b9f1b830a4fe92c5f492e45a85067e97f28fc9aa
Joshua Lynch <jklynch@email.arizona.edu>
2017-04-19 11:53:50 -0700

    Added missing environment variable IMICROBE_CONF and redirected mojo output to files in the imicrobe home directory

--- files/start_imicrobe_mojo.sh
+++ files/start_imicrobe_mojo.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+export IMICROBE_CONF=/home/imicrobe/imicrobe/lib/conf/imicrobe.yaml
+
 cd /home/imicrobe/imicrobe/lib
-/home/imicrobe/perl5/perlbrew/perls/perl-5.25.10/bin/hypnotoad mojo/script/imicrobe
+/home/imicrobe/perl5/perlbrew/perls/perl-5.25.10/bin/hypnotoad mojo/script/imicrobe 2&> /home/imicrobe/imicrobe_mojo.out
